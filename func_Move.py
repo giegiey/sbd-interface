8ce439f9449bf4e3c5652b94bf487ced06daa889
fahmi <fahmi@closed-loop.biz>
2022-11-10 17:05:09 +0800

    Stop conveyor after induct

--- func_Move.py
+++ func_Move.py
@@ -8,14 +8,15 @@ def State_Move():
     var_global.bBCchecking = True
     var_global.bBCpass = False
 
-    if var_global.bNewCommand:
+    if var_global.bNewCommand:  # 001,005
         var_global.bNewCommand = False
         if var_global.Com2 == 0:
             var_global.SocketTxMsg1 = "002,000\n"
         elif var_global.Com2 == 4:
-            print (datetime.now().strftime("%H:%M:%S.%f") + " Move    : Reset, Return to idle")
-            GPIO.output(ConREV,GPIO.HIGH)
-            GPIO.output(ConFWD,GPIO.HIGH)
+            print(datetime.now().strftime("%H:%M:%S.%f") +
+                  " Move    : Reset, Return to idle")
+            GPIO.output(ConREV, GPIO.HIGH)
+            GPIO.output(ConFWD, GPIO.HIGH)
             var_global.bGetWeight = False
             var_global.BarCodeOld = ""
             var_global.WeightOld = "00000"
@@ -27,100 +28,120 @@ def State_Move():
             var_global.bBlinkGreen = False
             var_global.bBlinkRed = False
             var_global.bBlinkRedStop = True
-            GPIO.output(LEDred,GPIO.HIGH)
-            GPIO.output(LEDblue,GPIO.HIGH)
-            GPIO.output(LEDgreen,GPIO.LOW)
+            GPIO.output(LEDred, GPIO.HIGH)
+            GPIO.output(LEDblue, GPIO.HIGH)
+            GPIO.output(LEDgreen, GPIO.LOW)
         elif var_global.Com2 == 5:
-            var_global.bBCchecking = False
-            if var_global.Com3 == 1:
-                var_global.bBCpass = True
+                var_global.bBCchecking = False
+                if var_global.Com3 == 1:
+                    var_global.bBCpass = True
         elif var_global.Com2 == 6:
             if not var_global.bBlinkRed:
                 var_global.bBlinkRed = True
         elif var_global.Com2 == 8:
-            var_global.bBCheavyEnable = True 
+            var_global.bBCheavyEnable = True
         elif var_global.Com2 == 9:
             var_global.SocketTxMsg1 = var_global.SocketTxMsgOld
             var_global.bPrint = False
         elif var_global.Com2 == 100:
             if var_global.Com3 == 0:
-                print (datetime.now().strftime("%H:%M:%S.%f") + " State still in Idle")
+                print(datetime.now().strftime(
+                    "%H:%M:%S.%f") + " State still in Idle")
                 var_global.SocketTxMsg1 = "State still in Idle\n"
             else:
-                print (datetime.now().strftime("%H:%M:%S.%f") + " State --> Debug")
+                print(datetime.now().strftime(
+                    "%H:%M:%S.%f") + " State --> Debug")
                 var_global.SocketTxMsg1 = "State --> Debug\n"
                 var_global.State = 100
-        
+
     if var_global.BarCode1 != "":
         if len(var_global.BarCode1) == 10:
             if var_global.BarCode1 == var_global.BarCodeOld:
                 var_global.BCtime = datetime.now()
-                print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Repeated")
+                print(datetime.now().strftime("%H:%M:%S.%f") +
+                      " Move : BarCode Repeated")
             else:
                 if var_global.BarCodeOld == "":
                     var_global.BCtime = datetime.now()
                     var_global.BarCodeOld = var_global.BarCode1
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode New: " + var_global.BarCodeOld)
+                    print(datetime.now().strftime("%H:%M:%S.%f") +
+                          " Move : BarCode New: " + var_global.BarCodeOld)
                 else:
                     var_global.bBCmultiple = True
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Mutiple ")
+                    print(datetime.now().strftime("%H:%M:%S.%f") +
+                          " Move : BarCode Mutiple ")
         else:
             if len(var_global.BarCode1) == 6:
                 if var_global.BarCode1 == "012345":
                     var_global.bBCheavyFound = True
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Heavy ")
+                    print(datetime.now().strftime(
+                        "%H:%M:%S.%f") + " Move : BarCode Heavy ")
                 else:
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Incomplete ")
+                    print(datetime.now().strftime("%H:%M:%S.%f") +
+                          " Move : BarCode Incomplete ")
             else:
-                print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Incomplete ")
-        var_global.BarCode1 = ""   
-        
+                print(datetime.now().strftime("%H:%M:%S.%f") +
+                      " Move : BarCode Incomplete ")
+        var_global.BarCode1 = ""
+
     if var_global.BarCode2 != "":
         if len(var_global.BarCode2) == 10:
             if var_global.BarCode2 == var_global.BarCodeOld:
                 var_global.BCtime = datetime.now()
-                print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Repeated")
+                print(datetime.now().strftime("%H:%M:%S.%f") +
+                      " Move : BarCode Repeated")
             else:
                 if var_global.BarCodeOld == "":
                     var_global.BCtime = datetime.now()
                     var_global.BarCodeOld = var_global.BarCode2
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode New: " + var_global.BarCodeOld)
+                    print(datetime.now().strftime("%H:%M:%S.%f") +
+                          " Move : BarCode New: " + var_global.BarCodeOld)
                 else:
                     var_global.bBCmultiple = True
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Mutiple ")
+                    print(datetime.now().strftime("%H:%M:%S.%f") +
+                          " Move : BarCode Mutiple ")
         else:
             if len(var_global.BarCode2) == 6:
                 if var_global.BarCode2 == "012345":
                     var_global.bBCheavyFound = True
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Heavy ")
+                    print(datetime.now().strftime(
+                        "%H:%M:%S.%f") + " Move : BarCode Heavy ")
                 else:
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Incomplete ")
+                    print(datetime.now().strftime("%H:%M:%S.%f") +
+                          " Move : BarCode Incomplete ")
             else:
-                print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Incomplete ")
+                print(datetime.now().strftime("%H:%M:%S.%f") +
+                      " Move : BarCode Incomplete ")
         var_global.BarCode2 = ""
-                
+
     if var_global.BarCode3 != "":
         if len(var_global.BarCode3) == 10:
             if var_global.BarCode3 == var_global.BarCodeOld:
                 var_global.BCtime = datetime.now()
-                print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Repeated")
+                print(datetime.now().strftime("%H:%M:%S.%f") +
+                      " Move : BarCode Repeated")
             else:
                 if var_global.BarCodeOld == "":
                     var_global.BCtime = datetime.now()
                     var_global.BarCodeOld = var_global.BarCode3
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode New: " + var_global.BarCodeOld)
+                    print(datetime.now().strftime("%H:%M:%S.%f") +
+                          " Move : BarCode New: " + var_global.BarCodeOld)
                 else:
                     var_global.bBCmultiple = True
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Mutiple ")
+                    print(datetime.now().strftime("%H:%M:%S.%f") +
+                          " Move : BarCode Mutiple ")
         else:
             if len(var_global.BarCode3) == 6:
                 if var_global.BarCode3 == "012345":
                     var_global.bBCheavyFound = True
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Heavy ")
+                    print(datetime.now().strftime(
+                        "%H:%M:%S.%f") + " Move : BarCode Heavy ")
                 else:
-                    print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Incomplete ")
+                    print(datetime.now().strftime("%H:%M:%S.%f") +
+                          " Move : BarCode Incomplete ")
             else:
-                print (datetime.now().strftime("%H:%M:%S.%f") + " Move : BarCode Incomplete ")
+                print(datetime.now().strftime("%H:%M:%S.%f") +
+                      " Move : BarCode Incomplete ")
         var_global.BarCode3 = ""
 
 # ------------------------------
@@ -140,10 +161,18 @@ def State_Move():
               " Move 1 : Conveyor Foward ")
         GPIO.output(ConREV, GPIO.HIGH)
         GPIO.output(ConFWD, GPIO.LOW)
+        var_global.startTime = datetime.now()
         var_global.iStep += 1
 
 # ------------------------------
     elif var_global.iStep == 2:
+        #timeout after no response then proceed
+        diffTime = datetime.now() - var_global.startTime
+        if ((diffTime.seconds*1000) + (diffTime.microseconds/1000)) >= sTimeout:
+            print(datetime.now().strftime("%H:%M:%S.%f") +
+                  " Move 11 : Timeout finish cancel and conveyor stop ")
+            GPIO.output(ConFWD, GPIO.HIGH)
+            var_global.iStep = 99
         if GPIO.input(OutputSen) == 0:
             if GPIO.input(OutputSen) == 0:
                 print(datetime.now().strftime("%H:%M:%S.%f") +
@@ -168,13 +197,12 @@ def State_Move():
     elif var_global.iStep == 4:
         diffTime = datetime.now() - var_global.startTime
         if ((diffTime.seconds * 1000) + (diffTime.microseconds / 1000)) >= tBagStop:
-            # GPIO.output(ConFWD,GPIO.HIGH)
+            GPIO.output(ConFWD, GPIO.HIGH)
             if var_global.bBCmultiple:
                 print(datetime.now().strftime("%H:%M:%S.%f") +
                       " Move  4 : Error, Multiple BarCode ")
                 if (var_global.nRetry <= 0):
                     var_global.SocketTxMsg1 = "002,003,007\n"
-                errorCode = 1
                 var_global.iStep += 1
             else:
                 if var_global.BarCodeOld != "":
@@ -183,7 +211,6 @@ def State_Move():
                             print(datetime.now().strftime("%H:%M:%S.%f") +
                                   " Move  4 : Success with BC Heavy" + var_global.BarCodeOld)
                             var_global.SocketTxMsg1 = "002,002," + var_global.BarCodeOld + "\n"
-                            errorCode = 0
                             var_global.iStep = 10
                         else:
                             print(datetime.now().strftime("%H:%M:%S.%f") +
@@ -195,7 +222,6 @@ def State_Move():
                         print(datetime.now().strftime("%H:%M:%S.%f") +
                               " Move  4 : Success " + var_global.BarCodeOld)
                         var_global.SocketTxMsg1 = "002,002," + var_global.BarCodeOld + "\n"
-                        errorCode = 0
                         var_global.iStep = 10
                 else:
                     print(datetime.now().strftime("%H:%M:%S.%f") +
@@ -218,8 +244,8 @@ def State_Move():
 
 # ------------------------------
     elif var_global.iStep == 6:
-        if GPIO.input(InputSen) == 1:
-            if GPIO.input(InputSen) == 1:
+        if GPIO.input(OutputSen) == 1:
+            if GPIO.input(OutputSen) == 1:
                 print(datetime.now().strftime("%H:%M:%S.%f") +
                       " Move  6 : Sensor no luggage")
                 var_global.startTime = datetime.now()
@@ -244,28 +270,54 @@ def State_Move():
     elif var_global.iStep == 10:
         if var_global.bBCchecking == False:
             if var_global.bBCpass == True:
+                var_global.bBCpass = True
                 print(datetime.now().strftime("%H:%M:%S.%f") +
-                      " Move 10 : Conveyor Foward ")
+                    " Move 10 : Conveyor Forward ")
                 GPIO.output(ConFWD, GPIO.LOW)
                 GPIO.output(ConREV, GPIO.HIGH)
                 var_global.startTime = datetime.now()
                 var_global.iStep += 1
             else:
-                print(datetime.now().strftime("%H:%M:%S.%f") +
-                      " Move 10 : Barcode Server Check Fail ")
+                print(datetime.now().strftime("%H:%M:%S.%f") + " Move 10 : Barcode Server Check Fail ")
                 var_global.nRetry = 0
                 var_global.iStep = 5
 
 # ------------------------------
     elif var_global.iStep == 11:
+        #timeout after no response then proceed
+        diffTime = datetime.now() - var_global.startTime
+        if ((diffTime.seconds*1000) + (diffTime.microseconds/1000)) >= sTimeout:
+            print(datetime.now().strftime("%H:%M:%S.%f") +
+                  " Move 11 : Timeout finish so proceed ")
+            var_global.iStep += 1
+        # check bag pass thru output sensor
+        # check output sensor for clear
+        # input/output sensor 1: clear 0: block
+        if GPIO.input(OutputSen) == 1:
+            print(datetime.now().strftime("%H:%M:%S.%f") +
+                  " Move 11 : Check output sensor for clear ")
+            var_global.iStep += 1
+
+# ------------------------------
+    elif var_global.iStep == 12:
         diffTime = datetime.now() - var_global.startTime
         if ((diffTime.seconds*1000) + (diffTime.microseconds/1000)) >= tBagMoveCont:
             GPIO.output(ConFWD, GPIO.HIGH)
             print(datetime.now().strftime("%H:%M:%S.%f") +
-                  " Move 11 : Conveyor stop ")
+                  " Move 12 : Conveyor stop ")
             var_global.iStep = 100
 
 # ------------------------------
+# Error no response
+    elif var_global.iStep == 99:  # Intrusion red light alert
+        GPIO.output(LEDred, GPIO.LOW)
+        GPIO.output(LEDgreen, GPIO.HIGH)
+        var_global.bBlinkRed = True
+        var_global.SocketTxMsg1 = "002,003,004\n"
+        var_global.iStep += 1
+
+# ------------------------------
+# Error intrusion sensor
     elif var_global.iStep == 99:  # Intrusion red light alert
         GPIO.output(LEDred, GPIO.LOW)
         GPIO.output(LEDgreen, GPIO.HIGH)
@@ -289,4 +341,3 @@ def State_Move():
 
     if var_global.SocketTxMsg1 != "":
         var_global.SocketTxMsgOld = var_global.SocketTxMsg1
-
